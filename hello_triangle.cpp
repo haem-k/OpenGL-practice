8c73bd6614e03a9a053a15175447ab6e6747b9b9
Haemin <rlagpals@gmail.com>
2022-11-06 22:13:43 +0900

    Added vertex shader

--- hello_triangle.cpp
+++ hello_triangle.cpp
@@ -2,6 +2,15 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// Vertex shader
+const char *vertexShaderSource = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 int main()
 {
     /* Send vertex data to GPU */
@@ -30,10 +39,23 @@ int main()
     // GL_STATIC_DRAW: data set only once and used many times
     // GL_DYNAMIC_DRAW: data changed a lot and used many times -> graphics card will place the data in memory that allows for faster writes
     // ex. position data of the triangle does not change, used a lot, stays the same for every render call -> GL_STATIC_DRAW
-    
-    /* Vertex Shader */
-    // Write a vertex shader in the shader language GLSL,
-    // Compile the shader to use in our application
 
+
+    /* Compile vertex shader */
+    // OpenGL dynamically compile shader at run-time from its source code
+    unsigned int vertexShader;                                      // ID for shader object
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);                // provide type of shader we want to create
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);     // second argument: how many strings we are passing as source code, which is 1 for now
+    glCompileShader(vertexShader);
+
+    // * Checking if the shader compilation is successfull
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 }
 
